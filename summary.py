a383698cd877cc33c2878584e4dd05ddf92efb54
chensheng1213 <134072174+chensheng1213@users.noreply.github.com>
2023-10-27 19:33:50 +0800

    summary.py

--- summary.py
+++ summary.py
@@ -8,15 +8,12 @@ from nets.deeplabv3_plus import DeepLab
 if __name__ == "__main__":
     input_shape     = [512, 512]
     num_classes     = 5
-    backbone        = 'xception'
-
+    backbone        = 'mobilenetv3_large'
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model   = DeepLab(num_classes=num_classes, backbone=backbone, downsample_factor=16, pretrained=False).to(device)
     summary(model, (3, input_shape[0], input_shape[1]))
-
     dummy_input     = torch.randn(1, 3, input_shape[0], input_shape[1]).to(device)
     flops, params   = profile(model.to(device), (dummy_input, ), verbose=False)
-
     flops           = flops * 2
     flops, params   = clever_format([flops, params], "%.3f")
     print('Total GFLOPS: %s' % (flops))
