8089410fd633e4d4775272a5acf7a94a03bfb1e3
chensheng1213 <134072174+chensheng1213@users.noreply.github.com>
2023-10-27 20:23:29 +0800

    mobilenetv2.py

--- nets/mobilenetv2.py
+++ nets/mobilenetv2.py
@@ -12,7 +12,6 @@ def conv_bn(inp, oup, stride):
         nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
         BatchNorm2d(oup),
         nn.ReLU6(inplace=True),
-
     )
 
 def conv_1x1_bn(inp, oup):
@@ -28,31 +27,25 @@ class InvertedResidual(nn.Module):
         super(InvertedResidual, self).__init__()
         self.stride = stride
         assert stride in [1, 2]
-
         hidden_dim = round(inp * expand_ratio)
         self.use_res_connect = self.stride == 1 and inp == oup
 
         if expand_ratio == 1:
             self.conv = nn.Sequential(
-
                 nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
                 BatchNorm2d(hidden_dim),
                 nn.ReLU6(inplace=True),
-
                 nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
                 BatchNorm2d(oup),
             )
         else:
             self.conv = nn.Sequential(
-
                 nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
                 BatchNorm2d(hidden_dim),
                 nn.ReLU6(inplace=True),
-
                 nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
                 BatchNorm2d(hidden_dim),
                 nn.ReLU6(inplace=True),
-
                 nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
                 BatchNorm2d(oup),
             )
@@ -70,7 +63,6 @@ class MobileNetV2(nn.Module):
         input_channel = 32
         last_channel = 1280
         interverted_residual_setting = [
-
             [1, 16, 1, 1],
             [6, 24, 2, 2],
             [6, 32, 3, 2],
@@ -79,13 +71,10 @@ class MobileNetV2(nn.Module):
             [6, 160, 3, 2],
             [6, 320, 1, 1],
         ]
-
         assert input_size % 32 == 0
         input_channel = int(input_channel * width_mult)
         self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
-
         self.features = [conv_bn(3, input_channel, 2)]
-
         for t, c, n, s in interverted_residual_setting:
             output_channel = int(c * width_mult)
             for i in range(n):
@@ -94,15 +83,12 @@ class MobileNetV2(nn.Module):
                 else:
                     self.features.append(block(input_channel, output_channel, 1, expand_ratio=t))
                 input_channel = output_channel
-
         self.features.append(conv_1x1_bn(input_channel, self.last_channel))
         self.features = nn.Sequential(*self.features)
-
         self.classifier = nn.Sequential(
             nn.Dropout(0.2),
             nn.Linear(self.last_channel, n_class),
         )
-
         self._initialize_weights()
 
     def forward(self, x):
@@ -126,7 +112,6 @@ class MobileNetV2(nn.Module):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
-
 def load_url(url, model_dir='./model_data', map_location=None):
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
@@ -142,4 +127,3 @@ def mobilenetv2(pretrained=False, **kwargs):
     if pretrained:
         model.load_state_dict(load_url('https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/mobilenet_v2.pth.tar'), strict=False)
     return model
-
