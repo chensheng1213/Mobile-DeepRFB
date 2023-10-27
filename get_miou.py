6ef7e0fd6855d07a189c84158f6d47b58df678cc
chensheng1213 <134072174+chensheng1213@users.noreply.github.com>
2023-10-27 19:16:01 +0800

    get_miou.py

--- get_miou.py
+++ get_miou.py
@@ -7,28 +7,20 @@ from deeplab import DeeplabV3
 from utils.utils_metrics import compute_mIoU, show_results
 
 if __name__ == "__main__":
-
     miou_mode       = 0
-
     num_classes     = 5
-
     name_classes    = ["soil", "bedrock", "sand", "big rock", "background"]
-
     VOCdevkit_path  = 'VOCdevkit'
-
     image_ids       = open(os.path.join(VOCdevkit_path, "VOC/ImageSets/Segmentation/val.txt"),'r').read().splitlines()
     gt_dir          = os.path.join(VOCdevkit_path, "VOC/SegmentationClass/")
     miou_out_path   = "miou_out/AI4_miou_out/mobile_RFB_miou_out"
     pred_dir        = os.path.join(miou_out_path, 'detection-results')
-
     if miou_mode == 0 or miou_mode == 1:
         if not os.path.exists(pred_dir):
-            os.makedirs(pred_dir)
-            
+            os.makedirs(pred_dir)   
         print("Load model.")
         deeplab = DeeplabV3()
         print("Load model done.")
-
         print("Get predict result.")
         for image_id in tqdm(image_ids):
             image_path  = os.path.join(VOCdevkit_path, "VOC/JPEGImages/"+image_id+".jpg")
@@ -36,7 +28,6 @@ if __name__ == "__main__":
             image       = deeplab.get_miou_png(image)
             image.save(os.path.join(pred_dir, image_id + ".png"))
         print("Get predict result done.")
-
     if miou_mode == 0 or miou_mode == 2:
         print("Get miou.")
         hist, IoUs, PA_Recall, Precision, = compute_mIoU(gt_dir, pred_dir, image_ids, num_classes, name_classes)  # 执行计算mIoU的函数
