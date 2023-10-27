3f93053eea3444f90df2c5bceef42e1ed7a76c52
chensheng1213 <134072174+chensheng1213@users.noreply.github.com>
2023-10-27 20:12:23 +0800

    utils_metrics.py

--- utils/utils_metrics.py
+++ utils/utils_metrics.py
@@ -1,24 +1,19 @@
 import csv
 import os
 from os.path import join
-
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn.functional as F
 from PIL import Image
 
-
 def f_score(inputs, target, beta=1, smooth = 1e-5, threhold = 0.5):
     n, c, h, w = inputs.size()
     nt, ht, wt, ct = target.size()
     if h != ht and w != wt:
         inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
-        
     temp_inputs = torch.softmax(inputs.transpose(1, 2).transpose(2, 3).contiguous().view(n, -1, c),-1)
     temp_target = target.view(n, -1, ct)
-
-
     temp_inputs = torch.gt(temp_inputs, threhold).float()
     tp = torch.sum(temp_target[...,:-1] * temp_inputs, axis=[0,1])
     fp = torch.sum(temp_inputs                       , axis=[0,1]) - tp
@@ -28,11 +23,8 @@ def f_score(inputs, target, beta=1, smooth = 1e-5, threhold = 0.5):
     score = torch.mean(score)
     return score
 
-
 def fast_hist(a, b, n):
-
     k = (a >= 0) & (a < n)
-
     return np.bincount(n * a[k].astype(int) + b[k], minlength=n ** 2).reshape(n, n)  
 
 def per_class_iu(hist):
@@ -49,19 +41,12 @@ def per_Accuracy(hist):
 
 def compute_mIoU(gt_dir, pred_dir, png_name_list, num_classes, name_classes=None):  
     print('Num classes', num_classes)  
-
     hist = np.zeros((num_classes, num_classes))
-
     gt_imgs     = [join(gt_dir, x + ".png") for x in png_name_list]  
     pred_imgs   = [join(pred_dir, x + ".png") for x in png_name_list]  
-
     for ind in range(len(gt_imgs)): 
-
         pred = np.array(Image.open(pred_imgs[ind]))  
-
         label = np.array(Image.open(gt_imgs[ind]))  
-
-
         if len(label.flatten()) != len(pred.flatten()):  
             print(
                 'Skipping: len(gt) = {:d}, len(pred) = {:d}, {:s}, {:s}'.format(
@@ -70,7 +55,6 @@ def compute_mIoU(gt_dir, pred_dir, png_name_list, num_classes, name_classes=None
             continue
 
         hist += fast_hist(label.flatten(), pred.flatten(), num_classes)  
-
         if name_classes is not None and ind > 0 and ind % 10 == 0: 
             print('{:d} / {:d}: mIou-{:0.2f}%; mPA-{:0.2f}%; Accuracy-{:0.2f}%'.format(
                     ind, 
@@ -80,7 +64,6 @@ def compute_mIoU(gt_dir, pred_dir, png_name_list, num_classes, name_classes=None
                     100 * per_Accuracy(hist)
                 )
             )
-
     IoUs        = per_class_iu(hist)
     PA_Recall   = per_class_PA_Recall(hist)
     Precision   = per_class_Precision(hist)
@@ -89,7 +72,6 @@ def compute_mIoU(gt_dir, pred_dir, png_name_list, num_classes, name_classes=None
         for ind_class in range(num_classes):
             print('===>' + name_classes[ind_class] + ':\tIou-' + str(round(IoUs[ind_class] * 100, 2)) \
                 + '; Recall (equal to the PA)-' + str(round(PA_Recall[ind_class] * 100, 2))+ '; Precision-' + str(round(Precision[ind_class] * 100, 2)))
-
     print('===> mIoU: ' + str(round(np.nanmean(IoUs) * 100, 2)) + '; mPA: ' + str(round(np.nanmean(PA_Recall) * 100, 2)) + '; Accuracy: ' + str(round(per_Accuracy(hist) * 100, 2)))  
     return np.array(hist, np.int), IoUs, PA_Recall, Precision
 
@@ -140,8 +122,6 @@ def show_results(miou_out_path, hist, IoUs, PA_Recall, Precision, name_classes, 
     draw_plot_func(Precision, name_classes, "mean Precision = {0:.2f}%".format(np.nanmean(Precision)*100), "Precision", \
         os.path.join(miou_out_path, "Precision.png"), tick_font_size = tick_font_size, plt_show = False)
     print("Save Precision out to " + os.path.join(miou_out_path, "Precision.png"))
-
-
 
     with open(os.path.join(miou_out_path, "confusion_matrix.csv"), 'w', newline='') as f:
         writer          = csv.writer(f)
