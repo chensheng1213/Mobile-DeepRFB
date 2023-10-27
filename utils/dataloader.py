db4a6d50b198c0570ef10702f1e965c24242d87f
chensheng1213 <134072174+chensheng1213@users.noreply.github.com>
2023-10-27 19:44:24 +0800

    dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -5,7 +5,6 @@ import numpy as np
 import torch
 from PIL import Image
 from torch.utils.data.dataset import Dataset
-
 from utils.utils import cvtColor, preprocess_input
 
 
@@ -25,18 +24,14 @@ class DeeplabDataset(Dataset):
     def __getitem__(self, index):
         annotation_line = self.annotation_lines[index]
         name            = annotation_line.split()[0]
-
         jpg         = Image.open(os.path.join(os.path.join(self.dataset_path, "VOC/JPEGImages"), name + ".jpg"))
         png         = Image.open(os.path.join(os.path.join(self.dataset_path, "VOC/SegmentationClass"), name + ".png"))
-
         jpg, png    = self.get_random_data(jpg, png, self.input_shape, random = self.train)
         jpg         = np.transpose(preprocess_input(np.array(jpg, np.float64)), [2,0,1])
         png         = np.array(png)
         png[png >= self.num_classes] = self.num_classes
-
         seg_labels  = np.eye(self.num_classes + 1)[png.reshape([-1])]
         seg_labels  = seg_labels.reshape((int(self.input_shape[0]), int(self.input_shape[1]), self.num_classes + 1))
-
         return jpg, png, seg_labels
 
     def rand(self, a=0, b=1):
@@ -45,25 +40,20 @@ class DeeplabDataset(Dataset):
     def get_random_data(self, image, label, input_shape, jitter=.3, hue=.1, sat=0.7, val=0.3, random=True):
         image   = cvtColor(image)
         label   = Image.fromarray(np.array(label))
-
         iw, ih  = image.size
         h, w    = input_shape
-
         if not random:
             iw, ih  = image.size
             scale   = min(w/iw, h/ih)
             nw      = int(iw*scale)
             nh      = int(ih*scale)
-
             image       = image.resize((nw,nh), Image.BICUBIC)
             new_image   = Image.new('RGB', [w, h], (128,128,128))
             new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-
             label       = label.resize((nw,nh), Image.NEAREST)
             new_label   = Image.new('L', [w, h], (0))
             new_label.paste(label, ((w-nw)//2, (h-nh)//2))
             return new_image, new_label
-
 
         new_ar = iw/ih * self.rand(1-jitter,1+jitter) / self.rand(1-jitter,1+jitter)
         scale = self.rand(0.25, 2)
@@ -89,14 +79,11 @@ class DeeplabDataset(Dataset):
         new_label.paste(label, (dx, dy))
         image = new_image
         label = new_label
-
         image_data      = np.array(image, np.uint8)
-
 
         blur = self.rand() < 0.25
         if blur: # if blur is none
             image_data = cv2.GaussianBlur(image_data, (5, 5), 0)
-
 
         rotate = self.rand() < 0.25
         if rotate: 
@@ -106,9 +93,7 @@ class DeeplabDataset(Dataset):
             image_data  = cv2.warpAffine(image_data, M, (w, h), flags=cv2.INTER_CUBIC, borderValue=(128,128,128))
             label       = cv2.warpAffine(np.array(label, np.uint8), M, (w, h), flags=cv2.INTER_NEAREST, borderValue=(0))
 
-
         r               = np.random.uniform(-1, 1, 3) * [hue, sat, val] + 1
-
         hue, sat, val   = cv2.split(cv2.cvtColor(image_data, cv2.COLOR_RGB2HSV))
         dtype           = image_data.dtype
 
@@ -116,13 +101,9 @@ class DeeplabDataset(Dataset):
         lut_hue = ((x * r[0]) % 180).astype(dtype)
         lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
         lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
-
         image_data = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
         image_data = cv2.cvtColor(image_data, cv2.COLOR_HSV2RGB)
-        
         return image_data, label
-
-
 
 def deeplab_dataset_collate(batch):
     images      = []
