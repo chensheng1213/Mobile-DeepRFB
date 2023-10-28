96b1a24a976e55895c526f1c3dccd4ddc7509f5f
chensheng1213 <134072174+chensheng1213@users.noreply.github.com>
2023-10-28 22:40:22 +0800

    dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -24,14 +24,21 @@ class DeeplabDataset(Dataset):
     def __getitem__(self, index):
         annotation_line = self.annotation_lines[index]
         name            = annotation_line.split()[0]
+        
         jpg         = Image.open(os.path.join(os.path.join(self.dataset_path, "VOC/JPEGImages"), name + ".jpg"))
         png         = Image.open(os.path.join(os.path.join(self.dataset_path, "VOC/SegmentationClass"), name + ".png"))
+        
         jpg, png    = self.get_random_data(jpg, png, self.input_shape, random = self.train)
+        
         jpg         = np.transpose(preprocess_input(np.array(jpg, np.float64)), [2,0,1])
         png         = np.array(png)
         png[png >= self.num_classes] = self.num_classes
+        #-------------------------------------------------------#
+        #   Converted to one_hot
+        #-------------------------------------------------------#
         seg_labels  = np.eye(self.num_classes + 1)[png.reshape([-1])]
         seg_labels  = seg_labels.reshape((int(self.input_shape[0]), int(self.input_shape[1]), self.num_classes + 1))
+      
         return jpg, png, seg_labels
 
     def rand(self, a=0, b=1):
@@ -40,21 +47,30 @@ class DeeplabDataset(Dataset):
     def get_random_data(self, image, label, input_shape, jitter=.3, hue=.1, sat=0.7, val=0.3, random=True):
         image   = cvtColor(image)
         label   = Image.fromarray(np.array(label))
+        #------------------------------#
+        #   Get the height and width of the image and the height and width of the target
+        #------------------------------#
         iw, ih  = image.size
         h, w    = input_shape
+        
         if not random:
             iw, ih  = image.size
             scale   = min(w/iw, h/ih)
             nw      = int(iw*scale)
             nh      = int(ih*scale)
+            
             image       = image.resize((nw,nh), Image.BICUBIC)
             new_image   = Image.new('RGB', [w, h], (128,128,128))
             new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+            
             label       = label.resize((nw,nh), Image.NEAREST)
             new_label   = Image.new('L', [w, h], (0))
             new_label.paste(label, ((w-nw)//2, (h-nh)//2))
             return new_image, new_label
-
+            
+        #------------------------------------------#
+        #   Scale the image
+        #------------------------------------------#
         new_ar = iw/ih * self.rand(1-jitter,1+jitter) / self.rand(1-jitter,1+jitter)
         scale = self.rand(0.25, 2)
         if new_ar < 1:
@@ -66,11 +82,17 @@ class DeeplabDataset(Dataset):
         image = image.resize((nw,nh), Image.BICUBIC)
         label = label.resize((nw,nh), Image.NEAREST)
 
+        #------------------------------------------#
+        #   Flip Image
+        #------------------------------------------#
         flip = self.rand()<.5
         if flip: 
             image = image.transpose(Image.FLIP_LEFT_RIGHT)
             label = label.transpose(Image.FLIP_LEFT_RIGHT)
 
+        #------------------------------------------#
+        #   Graybar the excess of the image
+        #------------------------------------------#
         dx = int(self.rand(0, w-nw))
         dy = int(self.rand(0, h-nh))
         new_image = Image.new('RGB', (w,h), (128,128,128))
@@ -79,12 +101,19 @@ class DeeplabDataset(Dataset):
         new_label.paste(label, (dx, dy))
         image = new_image
         label = new_label
+       
         image_data      = np.array(image, np.uint8)
-
+        
+        #------------------------------------------#
+        #   Gaussian blur
+        #------------------------------------------#
         blur = self.rand() < 0.25
         if blur: # if blur is none
             image_data = cv2.GaussianBlur(image_data, (5, 5), 0)
 
+        #------------------------------------------#
+        #   rotating
+        #------------------------------------------#
         rotate = self.rand() < 0.25
         if rotate: 
             center      = (w // 2, h // 2)
@@ -93,6 +122,9 @@ class DeeplabDataset(Dataset):
             image_data  = cv2.warpAffine(image_data, M, (w, h), flags=cv2.INTER_CUBIC, borderValue=(128,128,128))
             label       = cv2.warpAffine(np.array(label, np.uint8), M, (w, h), flags=cv2.INTER_NEAREST, borderValue=(0))
 
+        #---------------------------------#
+        #   Color gamut transformation of the image
+        #---------------------------------#
         r               = np.random.uniform(-1, 1, 3) * [hue, sat, val] + 1
         hue, sat, val   = cv2.split(cv2.cvtColor(image_data, cv2.COLOR_RGB2HSV))
         dtype           = image_data.dtype
