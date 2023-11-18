8ba3bc32b0911005899c93a71b93d797db4d5a6b
chensheng1213 <134072174+chensheng1213@users.noreply.github.com>
2023-11-18 18:03:16 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -9,11 +9,32 @@ from deeplab import DeeplabV3
 if __name__ == "__main__":
     deeplab = DeeplabV3()
     mode = "predict"
+    '''
+    mode Specifies the mode of the test:
+    'predict' represents a single image prediction. If you want to modify the prediction process, such as saving images, capturing objects, etc., you can first read the detailed comments below
+    'video' indicates video detection. Cameras or videos can be used for detection. For details, see the comments below.
+    'fps' stands for testing fps, and the image used is street.jpg in img, see the comments below for details.
+    'dir_predict' means to traverse the folder for detection and saving. By default, traverse the img folder and save the img_out folder. See the comments below for details.
+    'export_onnx' means that exporting the model as onnx requires pytorch1.7.1 or more.
+    '''
     count           = True
+    # count Specifies whether the pixel count (i.e. area) and scale calculation of the target are performed
+    # count is only valid if mode='predict'
     name_classes = ["background", "soil", "bedrock", "sand", "big rock"]
+    '''
+    'video_path' is used to specify the path of the video. video_path=0 indicates that the camera is detected
+    'video_save_path' indicates the path where the video is saved. If video_save_path="", the video is not saved
+    'video_fps' is the fps used to save the video
+     video_path, video_save_path, and video_fps are valid only when mode='video'
+    '''
     video_path      = 0
     video_save_path = ""
     video_fps       = 25.0
+    '''
+    'test_interval' is used to specify the number of image detections when measuring fps. Theoretically, the larger the test_interval, the more accurate the fps.
+    'fps_image_path' is used to specify the fps image for testing
+     test_interval and fps_image_path are valid only in mode='fps'
+    '''
     test_interval = 100
     fps_image_path  = "Test_img_mer/images/1n128623896eff0205p1548l0m1.jpg"
     dir_origin_path = "predict_out/mer/img/"
